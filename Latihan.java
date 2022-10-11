6978202e5fa431f04cfed63d931a8c3d62abe6b7
Jhannaa <115527667+Jhannaa@users.noreply.github.com>
2022-10-11 14:13:47 +0700

    Add files via upload

---
+++ Latihan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan;
+import java.lang.Math;
+/**
+ *
+ * @author hp
+ */
+public class Latihan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ini adalah angka keberuntungan anda hari ini : " + (int) (Math.floor(Math.random() * 1000)));
+        System.out.println("Jangan lupa dijadikan kompilasi 50 data diskrit.");
+    }
+    
+}
